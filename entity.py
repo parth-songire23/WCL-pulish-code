dc3fcc60356322a29a8117b01ff79c12bc323560
Parth Songire <111558783+parth-songire23@users.noreply.github.com>
2025-03-20 13:50:59 +0530

    Update entity.py

--- entity.py
+++ entity.py
@@ -1,104 +1,69 @@
 import numpy as np
 import math
-#from math_tool import *
 
 class UAV(object):
     """
-    UAV object with coordinate 
-    And with ULA antenas, default 8 
-    And limited power
-    And with fixed rotation angle
+    UAV (Unmanned Aerial Vehicle) with:
+    - Position (Coordinate)
+    - Beamforming Matrix
+    - Antennas
+    - Movement Capability
     """
-    def __init__(self, coordinate, index = 0, rotation = 0, ant_num=16, ant_type = 'ULA', max_movement_per_time_slot = 0.5):
-        """
-        coordinate is the init coordinate of UAV, meters, np.array
-        """
+    def __init__(self, coordinate, index=0, rotation=0, ant_num=16, ant_type='ULA', max_movement_per_time_slot=0.5):
         self.max_movement_per_time_slot = max_movement_per_time_slot
         self.type = 'UAV'
         self.coordinate = coordinate
         self.rotation = rotation
         self.ant_num = ant_num
         self.ant_type = ant_type
-        self.coor_sys = [np.array([1, 0, 0]), np.array([0, -1, 0]), np.array([0, 0, -1])]
         self.index = index
+        self.coor_sys = [np.array([1, 0, 0]), np.array([0, -1, 0]), np.array([0, 0, -1])]
 
-        # init beamforming matrix in UAV (must be inited in env.py)
+        # Initialize beamforming matrix (Power allocation)
         self.G = np.mat(np.zeros((ant_num, 1)))
         self.G_Pmax = 0
 
     def reset(self, coordinate):
-        """
-        reset UAV coordinate
-        """
+        """ Reset UAV position """
         self.coordinate = coordinate
-        
-    def update_coor_sys(self, delta_angle):
-        """
-        used in function move to update the relevant coordinate system 
-        """
-        self.rotation = self.rotation + delta_angle
-        coor_sys_x = np.array([\
-        math.cos(self.rotation),\
-        math.sin(self.rotation),\
-        0])
-        coor_sys_z = np.array([\
-        0,\
-        0,\
-        -1])
-        coor_sys_y = np.cross(coor_sys_z, coor_sys_x)
-        self.coor_sys = np.array([coor_sys_x,coor_sys_y,coor_sys_z])
-        
-    def update_coordinate(self, distance_delta_d, direction_fai):
-        """
-        used in function move to update UAV cordinate
-        """
+
+    def move(self, distance_delta_d, direction_fai, delta_angle=0):
+        """ Move UAV in 2D space """
         delta_x = distance_delta_d * math.cos(direction_fai)
         delta_y = distance_delta_d * math.sin(direction_fai)
         self.coordinate[0] += delta_x
         self.coordinate[1] += delta_y
 
-    def move(self, distance_delta_d, direction_fai, delta_angle = 0):
-        """
-        preform the 2D movement every step
-        """
-        self.update_coordinate(distance_delta_d, direction_fai)
-        self.update_coor_sys(delta_angle)
-
 class RIS(object):
     """
-    reconfigrable intelligent surface
-    with N reflecting elements, UPA, default 4 X 4 = 16
-    continues phase shift
+    Reconfigurable Intelligent Surface (RIS) with:
+    - Reflecting elements
+    - Position (Coordinate)
     """
-    def __init__(self, coordinate, coor_sys_z, index = 0, ant_num=36, ant_type = 'UPA'):
-        """
-        coordinate is the init coordinate of with N reflecting elements, meters, np.array
-        norm_vec is the normal vector of the reflecting direction
-        !!! ant_num Must be the square of a certain int number
-        """
+    def __init__(self, coordinate, coor_sys_z, index=0, ant_num=36, ant_type='UPA'):
         self.type = 'RIS'
         self.coordinate = coordinate
         self.ant_num = ant_num
         self.ant_type = ant_type
-        coor_sys_z = coor_sys_z / np.linalg.norm(coor_sys_z)
-        coor_sys_x = np.cross(coor_sys_z, np.array([0,0,1]))
-        coor_sys_x = coor_sys_x / np.linalg.norm(coor_sys_x)
-        coor_sys_y = np.cross(coor_sys_z, coor_sys_x)
-        self.coor_sys = [coor_sys_x,coor_sys_y,coor_sys_z]
         self.index = index
 
-        # init reflecting phase shift
-        self.Phi = np.mat(np.diag(np.ones(self.ant_num, dtype=complex)), dtype = complex)
+        coor_sys_z = coor_sys_z / np.linalg.norm(coor_sys_z)
+        coor_sys_x = np.cross(coor_sys_z, np.array([0, 0, 1]))
+        coor_sys_x = coor_sys_x / np.linalg.norm(coor_sys_x)
+        coor_sys_y = np.cross(coor_sys_z, coor_sys_x)
+        self.coor_sys = [coor_sys_x, coor_sys_y, coor_sys_z]
+
+        # Initialize reflecting phase shift
+        self.Phi = np.mat(np.diag(np.ones(self.ant_num, dtype=complex)), dtype=complex)
 
 class User(object):
     """
-    user with single antenas
+    User (Receiver) with:
+    - Position
+    - Single Antenna
+    - Capacity (Throughput)
     """
-    def __init__(self, coordinate, index, ant_num = 1, ant_type = 'single'):
-        """
-        coordinate is the init coordinate of user, meters, np.array
-        ant_num is the antenas number of user
-        """
+    def __init__(self, coordinate, index, ant_num=1, ant_type='single'):
         self.type = 'user'
         self.coordinate = coordinate
         self.ant_num = ant_num
@@ -106,45 +71,27 @@ class User(object):
         self.index = index
         self.coor_sys = [np.array([1, 0, 0]), np.array([0, 1, 0]), np.array([0, 0, 1])]
 
-        # init the capacity
+        # Communication capacity
         self.capacity = 0
         self.secure_capacity = 0
         self.QoS_constrain = 0
-        # init the comprehensive_channel, (must used in env.py to init)
+
+        # Signal processing properties
         self.comprehensive_channel = 0
-        # init receive noise sigma in dB
-        self.noise_power = -114
+        self.noise_power = -114  # Noise floor (dBm)
 
     def reset(self, coordinate):
-        """
-        reset user coordinate
-        """
+        """ Reset user position """
         self.coordinate = coordinate
-        
-    def update_coordinate(self, distance_delta_d, direction_fai):
-        """
-        used in function move to update UAV cordinate
-        """
-        delta_x = distance_delta_d * math.cos(direction_fai)
-        delta_y = distance_delta_d * math.sin(direction_fai)
-        self.coordinate[0] += delta_x
-        self.coordinate[1] += delta_y
 
-    def move(self, distance_delta_d, direction_fai):
-        """
-        preform the 2D movement every step
-        """
-        self.update_coordinate(distance_delta_d, direction_fai)
-        
 class Attacker(object):
     """
-    Attacker with single antenas
+    Attacker (Eavesdropper) with:
+    - Position
+    - Single Antenna
+    - Eavesdropping Capacity
     """
-    def __init__(self, coordinate, index, ant_num = 1, ant_type= 'single'):
-        """
-        coordinate is the init coordinate of Attacker, meters, np.array
-        ant_num is the antenas number of Attacker
-        """
+    def __init__(self, coordinate, index, ant_num=1, ant_type='single'):
         self.type = 'attacker'
         self.coordinate = coordinate
         self.ant_num = ant_num
@@ -152,30 +99,39 @@ class Attacker(object):
         self.index = index
         self.coor_sys = [np.array([1, 0, 0]), np.array([0, 1, 0]), np.array([0, 0, 1])]
 
-        # init the capacity, this is a K length np.array ,shape: (K,)
-        # represent the attack rate for kth user, (must init in env.py)
+        # Attack capacity (per user)
         self.capacity = 0
         self.comprehensive_channel = 0
-        # init receive noise sigma in dBmW
-        self.noise_power = -114
+        self.noise_power = -114  # Noise floor
 
     def reset(self, coordinate):
-        """
-        reset attacker coordinate
-        """
+        """ Reset attacker position """
         self.coordinate = coordinate
 
-    def update_coordinate(self, distance_delta_d, direction_fai):
-        """
-        used in function move to update UAV cordinate
-        """
-        delta_x = distance_delta_d * math.cos(direction_fai)
-        delta_y = distance_delta_d * math.sin(direction_fai)
-        self.coordinate[0] += delta_x
-        self.coordinate[1] += delta_y
+class Jammer(object):
+    """
+    Jammer with:
+    - Position (Coordinate)
+    - Interference Power
+    - Single Antenna
+    """
+    def __init__(self, coordinate, index, power=5, ant_num=1, ant_type='single'):
+        self.type = 'jammer'
+        self.coordinate = coordinate  # Position in space
+        self.index = index
+        self.ant_num = ant_num
+        self.ant_type = ant_type
+        self.power = power  # Jamming power (Watts)
+        self.coor_sys = [np.array([1, 0, 0]), np.array([0, 1, 0]), np.array([0, 0, 1])]
 
-    def move(self, distance_delta_d, direction_fai):
+    def reset(self, coordinate):
+        """ Reset jammer position """
+        self.coordinate = coordinate
+
+    def generate_interference(self, signal):
         """
-        preform the 2D movement every step
+        Add jamming interference to a given signal.
+        The interference is modeled as additive white Gaussian noise (AWGN).
         """
-        self.update_coordinate(distance_delta_d, direction_fai)
+        interference = self.power * np.random.normal(0, 1, signal.shape)
+        return signal + interference
