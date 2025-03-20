40358c54f60002b10b179b2dc5ed83abc666d40c
Parth Songire <111558783+parth-songire23@users.noreply.github.com>
2025-03-20 13:59:57 +0530

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -6,51 +6,72 @@ import time
 
 class DataManager(object):
     """
-    class to read and store simulation results
-    before use, please create a direction under current file path './data'
-    and must have a file 'init_location.xlsx' which contain the position of each entities
+    Handles reading and storing simulation results.
+    - Stores UAV state, beamforming, RIS coefficients, user capacity.
+    - Now also tracks **jamming power** over episodes.
     """
-    def __init__(self, store_list = ['beamforming_matrix', 'reflecting_coefficient', 'UAV_state', 'user_capacity'],file_path = './data', store_path = './data/storage'):
-        # 1 init location data
+
+    def __init__(self, store_list=None, file_path='./data', store_path='./data/storage'):
+        """
+        Initializes data storage system.
+        :param store_list: List of variables to store in simulation results.
+        """
+        if store_list is None:
+            store_list = ['beamforming_matrix', 'reflecting_coefficient', 
+                          'UAV_state', 'user_capacity', 'jamming_power']  # Added jamming power
+
         self.store_list = store_list
-        self.init_data_file = file_path + '/init_location.xlsx'
-        self.time_stemp = time.strftime('/%Y-%m-%d %H_%M_%S',time.localtime(time.time()))
-        self.store_path = store_path + self.time_stemp 
-        os.makedirs(self.store_path) 
-        # self.writer = pd.ExcelWriter(self.store_path + '/simulation_result.xlsx', engine='openpyxl')  # pylint: disable=abstract-class-instantiated 
+        self.init_data_file = os.path.join(file_path, 'init_location.xlsx')
+
+        # Create unique storage directory for each run
+        self.timestamp = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime())
+        self.store_path = os.path.join(store_path, self.timestamp)
+        os.makedirs(self.store_path, exist_ok=True)
+
         self.simulation_result_dic = {}
         self.init_format()
 
-    def save_file(self, episode_cnt = 0):
-        # when ended, auto save to .mat file
-        scipy.io.savemat(self.store_path + '/simulation_result_ep_' + str(episode_cnt) + '.mat', {'result_' + str(episode_cnt):self.simulation_result_dic})
+    def save_file(self, episode_cnt=0):
+        """
+        Saves simulation results to a .mat file after each episode.
+        """
+        file_name = f'simulation_result_ep_{episode_cnt}.mat'
+        scipy.io.savemat(os.path.join(self.store_path, file_name), 
+                         {'result_' + str(episode_cnt): self.simulation_result_dic})
         self.simulation_result_dic = {}
         self.init_format()
 
     def save_meta_data(self, meta_dic):
         """
-        save system and agent information
+        Saves metadata about the system and agent.
         """
-        scipy.io.savemat(self.store_path + '/meta_data.mat', {'meta_data': meta_dic})
-        
+        scipy.io.savemat(os.path.join(self.store_path, 'meta_data.mat'), 
+                         {'meta_data': meta_dic})
+
     def init_format(self):
         """
-        used only one time in env.py
+        Initializes empty storage dictionary.
         """
         for store_item in self.store_list:
-            self.simulation_result_dic.update({store_item:[]})
+            self.simulation_result_dic[store_item] = []
 
-    def read_init_location(self, entity_type = 'user', index = 0):
-        if entity_type == 'user' or 'attacker' or 'RIS' or 'RIS_norm_vec' or 'UAV':
-            return np.array([\
-            pd.read_excel(self.init_data_file, sheet_name=entity_type)['x'][index],\
-            pd.read_excel(self.init_data_file, sheet_name=entity_type)['y'][index],\
-            pd.read_excel(self.init_data_file, sheet_name=entity_type)['z'][index]])
+    def read_init_location(self, entity_type='user', index=0):
+        """
+        Reads initial location data from 'init_location.xlsx'.
+        Now correctly validates entity_type before reading.
+        """
+        valid_entities = {'user', 'attacker', 'RIS', 'RIS_norm_vec', 'UAV', 'jammer'}
+        if entity_type in valid_entities:
+            df = pd.read_excel(self.init_data_file, sheet_name=entity_type)
+            return np.array([df['x'][index], df['y'][index], df['z'][index]])
         else:
-            return None
-    
+            raise ValueError(f"Invalid entity type: {entity_type}. Must be one of {valid_entities}")
+
     def store_data(self, row_data, value_name):
         """
-        docstring
+        Stores simulation data (e.g., beamforming, capacity, jamming power).
         """
-        self.simulation_result_dic[value_name].append(row_data)
+        if value_name in self.simulation_result_dic:
+            self.simulation_result_dic[value_name].append(row_data)
+        else:
+            raise KeyError(f"'{value_name}' is not a valid storage key.")
